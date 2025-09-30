2382773fb906932d5d6d2371b6f4cc031eb9b82d
birds-are-corrupted <69503678+birds-are-corrupted@users.noreply.github.com>
2025-09-30 18:28:44 +0200

    elegant

--- src/quotes.rs
+++ src/quotes.rs
@@ -1,49 +1,31 @@
 pub mod quotes {
-    #[derive(PartialEq, Clone)]
+    use serde::Deserialize;
+
+    #[derive(PartialEq, Deserialize, Clone)]
     pub struct Entry {
         pub text: String,
         pub author: String,
         pub theme: String,
         pub probability: u8,
     }
+    #[derive(Deserialize)]
+    pub struct QuotesFile {
+        pub quote: Vec<Entry>,
+    }
     pub fn get_quotes() -> Vec<Entry> {
-        let mut m: Vec<Entry> = Vec::new();
-        let a = Entry {
-            text: "If debugging is the process of removing software bugs, then programming must be the process of putting them in".to_string(),
-            author: "Edsger W. Dijkstra".to_string(),
-            theme: "Cyberspace".to_string(),
-            probability: 5,
-        };
-        let b = Entry {
-            text: "Object-oriented programming is an exceptionally bad idea which could only have originated in California.".to_string(),
-            author: "Edsger W. Dijkstra".to_string(),
-            theme: "Cyberspace".to_string(),
-            probability: 5,
-        };
-        let c = Entry {
-            text: "The use of COBOL cripples the mind; its teaching should, therefore, be regarded as a criminal offense".to_string(),
-            author: "Edsger W. Dijkstra".to_string(),
-            theme: "Cyberspace".to_string(),
-            probability: 5,
-        };
-        let d = Entry {
-            text: "Computer science is no more about computers than astronomy is about telescopes"
-                .to_string(),
-            author: "Edsger W. Dijkstra".to_string(),
-            theme: "Cyberspace".to_string(),
-            probability: 5,
-        };
-        let e = Entry {
-            text: "It is practically impossible to teach good programming to students that have had a prior exposure to BASIC: as potential programmers they are mentally mutilated beyond hope of regeneration".to_string(),
-            author: "Edsger W. Dijkstra".to_string(),
-            theme: "Cyberspace".to_string(),
-            probability: 5,
-        };
-        m.push(a);
-        m.push(b);
-        m.push(c);
-        m.push(d);
-        m.push(e);
-        m
+        let quotes_from_file = include_str!("../quotes.toml");
+        toml::from_str::<QuotesFile>(quotes_from_file)
+            .map(|possible_quote| possible_quote.quote)
+            .unwrap_or_else(|_| {
+                vec![Entry {
+                    text: "No Quote Found".into(),
+                    author: "No just No".into(),
+                    theme: "No".into(),
+                    probability: 0,
+                }]
+            })
+            .iter()
+            .cloned()
+            .collect()
     }
 }
