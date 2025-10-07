0952580102cc03899a98793bc327ddac90de4ef8
travelling-merchant <69503678+travelling-merchant@users.noreply.github.com>
2025-10-08 00:20:24 +0200

    names what do they mean

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::usize;
 
 use leptos::prelude::*;
 mod quotes;
-use js_sys::Math;
+use js_sys::Math::random;
 
 #[component]
 fn App() -> impl IntoView {
     let quotes: Vec<quotes::quotes::Entry> = quotes::quotes::get_quotes();
     let total_quotes: usize = quotes.len();
-
-    let (quote_num, set_quite_num) = signal(0);
+    let starting_num = get_initial_random_index(total_quotes);
+    let (quote_num, set_quite_num) = signal(starting_num);
     let current_quote = Memo::new(move |_| {
         quotes
             .get(quote_num.get())
@@ -37,7 +37,7 @@ fn App() -> impl IntoView {
                 let new_quote_index = random_index(total_quotes,quote_num.get());
             set_quite_num.set(new_quote_index);
             }>
-        generate random quote
+            get awesome  quote
             </button>
             <button class="generateQuoteButton" on:click=copy_to_clipboard>
                 Copy to clipboard
@@ -57,12 +57,15 @@ fn main() {
     console_error_panic_hook::set_once();
     leptos::mount::mount_to_body(App)
 }
+
 fn random_index(max: usize, old_me_haha: usize) -> usize {
     loop {
-        let random = Math::random();
-        let n = (random * max as f64).floor() as usize;
+        let n = get_initial_random_index(max);
         if n != old_me_haha {
             return n;
         }
     }
+}
+fn get_initial_random_index(max: usize) -> usize {
+    (random() * max as f64).floor() as usize
 }
